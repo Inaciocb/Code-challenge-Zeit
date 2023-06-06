9a4c024c8135d739d36ca41f20c6e6d6c1b6ce84
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-06-06 20:46:53 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -139,21 +139,6 @@ const Curriculo: React.FC = () => {
         <div className="w-1/4 ml-8">
           <div className="my-8">
             <h2 className="text-2xl font-bold flex items-center">
-              <svg
-                aria-hidden="true"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                fill="none"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 mr-2"
-              >
-                <path
-                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </svg>
               Dados pessoais
             </h2>
             <ul className="text-brown-700">
